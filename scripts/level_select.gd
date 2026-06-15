309882f66072c728f02a42e9ea9e63f8b8e117fe
Crafty Jumper <blueghostbear@gmail.com>
2026-06-15 15:12:17 -0700

    did i fix the thing now

--- scripts/level_select.gd
+++ scripts/level_select.gd
@@ -51,7 +51,7 @@ func _process(delta: float) -> void:
 		await Global.sfx.finished
 		Global.fade_scene("res://scenes/level.tscn")
 	
-	center_level.position.x /= 75 * delta
+	center_level.position.x = lerp(center_level.position.x,0.0,delta * 12)
 	left_level.position.x = center_level.position.x-get_viewport_rect().size.x/4
 	right_level.position.x = center_level.position.x+get_viewport_rect().size.x/4
 	
